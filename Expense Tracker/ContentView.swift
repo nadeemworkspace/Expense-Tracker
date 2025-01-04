e68b2f2655421089844ec151508379e24d9ae1c9
Muhammed Nadeem <muhammednadeem989@gmail.com>
2025-01-04 15:11:14 +0530

    Update ContentView.swift
    commit testing

--- Expense Tracker/ContentView.swift
+++ Expense Tracker/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Text("This is a text label")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
