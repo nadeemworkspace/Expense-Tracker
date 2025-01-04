1634ea9ad78bb800718429a6fa815672aa10842f
Muhammed Nadeem <muhammednadeem989@gmail.com>
2025-01-04 15:08:08 +0530

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Muhammed Nadeem M A on 04/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
